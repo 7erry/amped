a16536cbf00bbb856eac9474fe553ac582003650
7erry <twalters@ATL-MBP-MAC60.local>
2014-09-11 16:38:46 -0400

    added hazelcast as a managed service

--- uifacade/src/main/java/com/amped/helloworld/HelloWorldApplication.java
+++ uifacade/src/main/java/com/amped/helloworld/HelloWorldApplication.java
@@ -9,6 +9,9 @@ import com.amped.helloworld.filter.DateNotSpecifiedFilterFactory;
 import com.amped.helloworld.health.TemplateHealthCheck;
 import com.amped.helloworld.resources.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.auth.basic.BasicAuthProvider;
@@ -33,6 +36,9 @@ import com.wordnik.swagger.jaxrs.reader.DefaultJaxrsApiReader;
 import com.amped.helloworld.routes.HelloRoute;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    final static Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
     public static void main(String[] args) throws Exception {
         new HelloWorldApplication().run(args);
     }
@@ -55,9 +61,9 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
         bootstrap.addCommand(new RenderCommand());
 
         // use assets as root
-	bootstrap.addBundle(new AssetsBundle("/assets","/ui"));
+        bootstrap.addBundle(new AssetsBundle("/assets", "/ui"));
 
-	// our helloworld api's
+        // our helloworld api's
         bootstrap.addBundle(new MigrationsBundle<HelloWorldConfiguration>() {
             @Override
             public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration configuration) {
@@ -65,10 +71,10 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
             }
         });
 
-	// demonstrate hibernate
+        // demonstrate hibernate
         bootstrap.addBundle(hibernateBundle);
 
-	// demonstrate view
+        // demonstrate view
         bootstrap.addBundle(new ViewBundle());
     }
 
@@ -78,50 +84,89 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
         final PersonDAO dao = new PersonDAO(hibernateBundle.getSessionFactory());
         final Template template = configuration.buildTemplate();
 
-	// health check
+        // health check template
         environment.healthChecks().register("template", new TemplateHealthCheck(template));
-        
-	// protected resource
-	environment.jersey().register(new BasicAuthProvider<>(new ExampleAuthenticator(), "SUPER SECRET STUFF"));
+
+        // protected resource
+        environment.jersey().register(new BasicAuthProvider<>(new ExampleAuthenticator(), "SUPER SECRET STUFF"));
         environment.jersey().register(new ProtectedResource());
 
-	// Filtered Resource
-	environment.jersey().getResourceConfig().getResourceFilterFactories().add(new DateNotSpecifiedFilterFactory());
+        // Filtered Resource
+        environment.jersey().getResourceConfig().getResourceFilterFactories().add(new DateNotSpecifiedFilterFactory());
         environment.jersey().register(new FilteredResource());
 
-	// view's
+        // view's
         environment.jersey().register(new ViewResource());
-        
 
-	// Rest API's
-	environment.jersey().register(new PeopleResource(dao));
+        // Rest API's
+        environment.jersey().register(new PeopleResource(dao));
         environment.jersey().register(new PersonResource(dao));
 
-	// Camel
-	try{
-          ManagedCamel camel = new ManagedCamel(new HelloRoute());
-          environment.lifecycle().manage(camel);
-          environment.jersey().register(new HelloWorldResource(template, camel.createProducer()));
-	}catch(Exception ex){
-	  System.out.println(ex);
-	}
+        // Camel
+        configureCamel(environment, template);
 
-	// Swagger
-	configureSwagger(environment);
+        // Hazelcast
+        configureHazelcast(configuration, environment, template);
+
+        // Swagger
+        configureSwagger(environment);
+    }
+
+    void configureCamel(Environment environment, Template template) {
+        try {
+            ManagedCamel camel = new ManagedCamel(new HelloRoute());
+            environment.lifecycle().manage(camel);
+            environment.jersey().register(new HelloWorldResource(template, camel.createProducer()));
+        } catch (Exception ex) {
+            logger.error(ex.getCause().getMessage());
+        }
     }
 
     void configureSwagger(Environment environment) {
-	String API_VERSION = "1.0.0";
-        // swagger setup (http://swagger.wordnik.com/) 
-        environment.jersey().register(new ApiListingResourceJSON()); 
-        environment.jersey().register(new ApiDeclarationProvider()); 
-        environment.jersey().register(new ResourceListingProvider()); 
-        ScannerFactory.setScanner(new DefaultJaxrsScanner()); 
-        ClassReaders.setReader(new DefaultJaxrsApiReader()); 
+        String API_VERSION = "1.0.0";
+        // swagger setup (http://swagger.wordnik.com/)
+        environment.jersey().register(new ApiListingResourceJSON());
+        environment.jersey().register(new ApiDeclarationProvider());
+        environment.jersey().register(new ResourceListingProvider());
+        ScannerFactory.setScanner(new DefaultJaxrsScanner());
+        ClassReaders.setReader(new DefaultJaxrsApiReader());
 
-        SwaggerConfig swaggerConfig = ConfigFactory.config(); 
-        swaggerConfig.setApiVersion(API_VERSION); 
-        swaggerConfig.setBasePath("http://local.amplify.com:8080"); 
+        SwaggerConfig swaggerConfig = ConfigFactory.config();
+        swaggerConfig.setApiVersion(API_VERSION);
+        String basePath = "http://localhost:8080";
+        swaggerConfig.setBasePath(basePath);
 
+        logger.info(environment.getApplicationContext().getContextPath());
+
+        logger.info("Swagger UI:\t" + basePath + "\n");
     }
+
+    void configureHazelcast(HelloWorldConfiguration configuration, Environment environment, Template template) {
+        try {
+            ManagedHazelcast hazelcast = new ManagedHazelcast(configuration);
+            environment.lifecycle().manage(hazelcast);
+        } catch (Exception ex) {
+            logger.error(ex.getCause().getMessage());
+        }
+    }
+
+/*
+    void configureMongo(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+	Mongo mongo = new Mongo(configuration.mongohost, configuration.mongoport);
+	DB db = mongo.getDB(configuration.mongodb);
+	MongoManaged mongoManaged = new MongoManaged(mongo);
+	environment.lifecycle().manage(mongoManaged);
+	environment..healthChecks().register("mongo", new MongoHealthCheck(mongo));
+
+	JacksonDBCollection<Saying, String> sayings = JacksonDBCollection.wrap(db.getCollection("sayings"), Saying.class, String.class);
+	if (sayings.find().count() == 0)
+	    sayings.insert(new Saying("Hello %s", "en"));
+
+	environment.jersey().register(new HelloWorldResource(new MongoSayingRepository(sayings)));
+
+	JacksonDBCollection<Story, String> stories = JacksonDBCollection.wrap(db.getCollection("stories"), Story.class, String.class);
+
+	environment.jersey().register(new StoryResource(stories));
+    }
+*/
 }
